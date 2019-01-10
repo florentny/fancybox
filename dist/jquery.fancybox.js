9cfaffba8a311ee84ce79144035f5fb7fb34c99e
Florent Charpin <dev@florent.us>
2019-01-10 18:29:45 -0500

    photo3

--- dist/jquery.fancybox.js
+++ dist/jquery.fancybox.js
@@ -40,7 +40,7 @@
     closeExisting: false,
 
     // Enable infinite gallery navigation
-    loop: false,
+    loop: true,
 
     // Horizontal space between slides
     gutter: 50,
@@ -74,8 +74,8 @@
       "zoom",
       //"share",
       "slideShow",
-      //"fullScreen",
-      //"download",
+      "fullScreen",
+      "download",
       "thumbs",
       "close"
     ],
@@ -215,15 +215,15 @@
         '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 10.6L6.6 5.2 5.2 6.6l5.4 5.4-5.4 5.4 1.4 1.4 5.4-5.4 5.4 5.4 1.4-1.4-5.4-5.4 5.4-5.4-1.4-1.4-5.4 5.4z"/></svg>' +
         "</button>",
 
-      // Arrows
+      // Arrows transform: scale(1,-1)
       arrowLeft:
         '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="{{PREV}}">' +
-        '<div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11.28 15.7l-1.34 1.37L5 12l4.94-5.07 1.34 1.38-2.68 2.72H19v1.94H8.6z"/></svg></div>' +
+        '<div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 492 492" ><path d="M198.608,246.104L382.664,62.04c5.068-5.056,7.856-11.816,7.856-19.024c0-7.212-2.788-13.968-7.856-19.032l-16.128-16.12C361.476,2.792,354.712,0,347.504,0s-13.964,2.792-19.028,7.864L109.328,227.008c-5.084,5.08-7.868,11.868-7.848,19.084c-0.02,7.248,2.76,14.028,7.848,19.112l218.944,218.932c5.064,5.072,11.82,7.864,19.032,7.864c7.208,0,13.964-2.792,19.032-7.864l16.124-16.12c10.492-10.492,10.492-27.572,0-38.06L198.608,246.104z"/></svg></div>' +
         "</button>",
 
       arrowRight:
         '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}">' +
-        '<div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.4 12.97l-2.68 2.72 1.34 1.38L19 12l-4.94-5.07-1.34 1.38 2.68 2.72H5v1.94z"/></svg></div>' +
+        '<div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 492 492" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z"/></div>' +
         "</button>",
 
       // This small close button will be appended to your html/inline/ajax content by default,
@@ -284,7 +284,7 @@
 
     slideShow: {
       autoStart: false,
-      speed: 3000
+      speed: 2000
     },
 
     thumbs: {
@@ -340,7 +340,7 @@
 
     // Clicked on the content
     clickContent: function(current, event) {
-      return current.type === "image" ? "zoom" : false;
+      return current.type === "image" ? "next" : false;
     },
 
     // Clicked on the slide
@@ -405,6 +405,19 @@
         DOWNLOAD: "Herunterladen",
         SHARE: "Teilen",
         ZOOM: "Vergr&ouml;&szlig;ern"
+      },
+      fr: {
+        CLOSE: "Close",
+        NEXT: "Next",
+        PREV: "Previous",
+        ERROR: "The requested content cannot be loaded. <br/> Please try again later.",
+        PLAY_START: "Start slideshow",
+        PLAY_STOP: "Pause slideshow",
+        FULL_SCREEN: "Full screen",
+        THUMBS: "Thumbnails",
+        DOWNLOAD: "Download",
+        SHARE: "Share",
+        ZOOM: "Zoom"
       }
     }
   };
@@ -2960,11 +2973,12 @@
       // Set caption
       if (caption && caption.length) {
         self.$caption = $caption;
-
+        var cap = caption.split("|");
+        var fcaption = cap[0] + "<div class=\"exif\">" + cap[1] + "</div>";
         $caption
           .children()
           .eq(0)
-          .html(caption);
+          .html(fcaption);
       } else {
         self.$caption = null;
       }
